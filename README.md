74e1b36b277edd3e5226a8659c971e3556d4e3b8
Sergey Romanov <xxsmotur@gmail.com>
2015-07-20 08:40:57 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# w2v-search-code
+Word2Vec based search on your code projects
+
+## Usage
+```python
+model = W2vSearch(['file1.py', 'file2.py'])
+print(model.search('function1'))
+```
---
+++ w2v.py
@@ -0,0 +1,45 @@
+import gensim
+from nltk.tokenize import word_tokenize, sent_tokenize
+import re
+import nltk
+from nltk.corpus import stopwords
+
+#nltk.download()
+
+
+class W2vSearch:
+    def __init__(self, data):
+        links = data
+        if type(data) is str:
+            links = [data]
+        self.model = gensim.models.Word2Vec(self._load(links), min_count=10)
+
+    def _load(self, links):
+        result = []
+        for link in links:
+            data = open(link, 'r').read()
+            result.extend(self._preprocess(data.decode('utf-8')))
+        return result
+
+    def _preprocess(self, str):
+        sents = [word_tokenize(" ".join(re.findall(r'\w+',t, flags=re.UNICODE | re.LOCALE)).lower())
+            for t in sent_tokenize(str.replace("'",""))]
+        stop = stopwords.words('english')
+        for s in range(len(sents)):
+            sents[s] = [w for w in sents[s] if w.lower() not in stop and len(w.lower()) > 2]
+        return sents
+
+    def similarity(self, word1, word2):
+        return self.model.similarity(word1, word2)
+
+    def search(self, word, negative=[]):
+        return self.model.most_similar(positive=[word], negative=negative)
+
+
+def preprocessing(str):
+    sents = [word_tokenize(" ".join(re.findall(r'\w+',t, flags=re.UNICODE | re.LOCALE)).lower())
+        for t in sent_tokenize(str.replace("'",""))]
+    stop = stopwords.words('english')
+    for s in range(len(sents)):
+        sents[s] = [w for w in sents[s] if w.lower() not in stop and len(w.lower()) > 2]
+    return sents
